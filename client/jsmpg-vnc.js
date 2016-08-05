87591795ec5cb920b6bad8c1f6e46baac0649325
Walter Berggren <walter.berggren@gmail.com>
2016-08-05 09:41:48 +0200

    Add scroll support

--- client/jsmpg-vnc.js
+++ client/jsmpg-vnc.js
@@ -16,8 +16,8 @@ var lastMouse = {x: 0, y: 0};
 if( mouseLock ) {
 	// FUCK YEAH, VENDOR PREFIXES. LOVE EM!
 	canvas.requestPointerLock = canvas.requestPointerLock ||
-		canvas.mozRequestPointerLock || 
-		canvas.webkitRequestPointerLock || 
+		canvas.mozRequestPointerLock ||
+		canvas.webkitRequestPointerLock ||
 		(function(){});
 }
 
@@ -33,6 +33,7 @@ var KEY_DOWN = 0x01,
 	MOUSE_1_UP = 0x0004,
 	MOUSE_2_DOWN = 0x0008,
 	MOUSE_2_UP = 0x0010;
+	MOUSEEVENTF_WHEEL = 0x0800;
 
 // struct input_key_t { uint16 type, uint16 state; uint16 key_code; }
 var sendKey = function(ev, action, key) {
@@ -40,10 +41,11 @@ var sendKey = function(ev, action, key) {
 	ev.preventDefault();
 };
 
-// struct input_mouse_t { uint16 type, uint16 flags; float32 x; float32 y; }
-var mouseDataBuffer = new ArrayBuffer(12);
+// struct input_mouse_t { uint16 type, uint16 flags; float32 x; float32 y; int16 amount }
+var mouseDataBuffer = new ArrayBuffer(16);
 var mouseDataTypeFlags = new Uint16Array(mouseDataBuffer, 0);
 var mouseDataCoords = new Float32Array(mouseDataBuffer, 4);
+var mouseScrollAmount = new Int32Array(mouseDataBuffer, 12);
 
 var sendMouse = function(ev, action) {
 	var type = 0;
@@ -51,19 +53,19 @@ var sendMouse = function(ev, action) {
 
 	if( action ) {
 		type |= INPUT_MOUSE_BUTTON;
-		
+
 		// Attempt to lock pointer at mouse1 down
 		if( mouseLock && action === MOUSE_1_DOWN ) {
 			canvas.requestPointerLock();
 		}
 	}
-	
+
 	// Only make relative mouse movements if no button is pressed
 	if( !action && mouseLock ) {
 		type |= INPUT_MOUSE_RELATIVE;
-		
+
 		var p = ev.changedTouches ? ev.changedTouches[0] : ev;
-		
+
 		// FUCK, DID I MENTION I LOOOOOVE VENDOR PREFIXES? SO USEFUL!
 		x = p.movementX || p.mozMovementX || p.webkitMovementX;
 		y = p.movementY || p.mozMovementY || p.webkitMovementY;
@@ -81,11 +83,11 @@ var sendMouse = function(ev, action) {
 	// button presses.
 	if( !mouseLock ) {
 		type |= INPUT_MOUSE_ABSOLUTE;
-		
+
 		var rect = canvas.getBoundingClientRect();
 		var scaleX = canvas.width / (rect.right-rect.left),
 			scaleY = canvas.height / (rect.bottom-rect.top);
-		
+
 		var p = event.changedTouches ? ev.changedTouches[0] : ev;
 		var x = (p.clientX - rect.left) * scaleX,
 			y = (p.clientY - rect.top) * scaleY;
@@ -95,7 +97,8 @@ var sendMouse = function(ev, action) {
 	mouseDataTypeFlags[1] = (action||0);
 	mouseDataCoords[0] = x;
 	mouseDataCoords[1] = y;
-	
+	mouseScrollAmount[0] = (ev.wheelDelta||0);
+
 	client.send(mouseDataBuffer);
 	ev.preventDefault();
 };
@@ -109,6 +112,7 @@ window.addEventListener('keyup', function(ev) { sendKey(ev, KEY_UP, ev.keyCode);
 canvas.addEventListener('mousemove', function(ev){ sendMouse(ev, null); }, false);
 canvas.addEventListener('mousedown', function(ev){ sendMouse(ev, ev.button == 2 ? MOUSE_2_DOWN : MOUSE_1_DOWN); }, false);
 canvas.addEventListener('mouseup', function(ev){ sendMouse(ev, ev.button == 2 ? MOUSE_2_UP : MOUSE_1_UP); }, false);
+canvas.addEventListener('mousewheel', function(ev){ sendMouse(ev, MOUSEEVENTF_WHEEL); }, false);
 
 // Touch
 canvas.addEventListener('touchstart', function(ev){
